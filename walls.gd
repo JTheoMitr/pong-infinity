69a08745e6d62b7a156e221c1b7b04bf673c5c41
JTheoMitr <jmitropoulos@alumni.berklee.edu>
2026-01-30 02:25:19 -0600

    multiplier 1 buff created
    base area and collision. next step is adding anim

--- walls.gd
+++ walls.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	
 	var main := get_tree().current_scene
 	if main.has_method("is_game_active") and main.is_game_active():

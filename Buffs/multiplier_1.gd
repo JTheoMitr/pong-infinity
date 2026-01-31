fd9e50084cbbe1b524ecd1ca78d3a2ffb0fc6694
JTheoMitr <jmitropoulos@alumni.berklee.edu>
2026-01-30 21:20:50 -0600

    red pyramid anim added
    initial multiplier1 tests passed yay

--- Buffs/multiplier_1.gd
+++ Buffs/multiplier_1.gd
@@ -4,3 +4,8 @@ extends Area2D
 
 func _on_area_entered(_area: Area2D) -> void:
 	print("multiplier_1")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("ball"):
+		print("multiplier_1")

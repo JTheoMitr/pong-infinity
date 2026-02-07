edd286c51f48f2aebd2629af0d1f9bde20acabbd
JTheoMitr <jmitropoulos@alumni.berklee.edu>
2026-02-07 17:51:40 -0600

    manual bounce in barriers
    and main menu updates

--- Buffs/multiplier_1.gd
+++ Buffs/multiplier_1.gd
@@ -5,7 +5,7 @@ const thunder_sfx = preload("res://Assets/SFX/sfx_thunder_2.tscn")
 @onready var lightning_anim: AnimatedSprite2D = $AnimatedSprite2D3
 @onready var spinning_pyramid_pink: AnimatedSprite2D = $AnimatedSprite2D
 @onready var spinning_pyramid_red: AnimatedSprite2D = $AnimatedSprite2D2
-
+@onready var barriers: Area2D = $Barriers
 signal ball_hit_multiplier_1(multiplier: Node)
 
 
@@ -15,6 +15,9 @@ func _ready() -> void:
 	var thunder_strike = thunder_sfx.instantiate()
 	get_parent().add_child(thunder_strike)
 	spinning_pyramid_pink.hide()
+	
+func _process(_delta: float) -> void:
+	barriers.rotation_degrees += 3
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("ball"):
@@ -37,3 +40,15 @@ func _on_timer_timeout() -> void:
 	
 func _on_timer_2_timeout() -> void:
 	self.queue_free()
+
+
+func _on_barriers_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("ball"):
+		return
+
+	# --- MANUAL BOUNCE ---
+	var to_ball: Vector2 = (body.global_position - global_position).normalized()
+	body.direction = body.direction.bounce(to_ball).normalized()
+	body.velocity = body.direction * body.base_speed
+
+	#emit_signal("ball_hit_barrier", self)
--- main_menu.gd
+++ main_menu.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 @onready var start_button = $CenterContainer/VBoxContainer/Button
-@onready var title = $CenterContainer/VBoxContainer/RichTextLabel
+@onready var title = $RichTextLabel
 @onready var panel = $Panel
 @onready var start_timer = $Timer
+@onready var button_1 = $CenterContainer/VBoxContainer/Button
+@onready var button_2 = $CenterContainer/VBoxContainer/Button2
 
 const ButtonClick = preload("res://Assets/SFX/sfx_button_click_1.tscn")
 
@@ -12,6 +14,8 @@ var panel_sliding: bool = false
 func _process(_delta: float) -> void:
 	if panel_sliding:
 		panel.global_position.y += 3
+		title.self_modulate.a -= .005
+		
 
 func _ready() -> void:
 	start_button.grab_focus()
@@ -21,6 +25,8 @@ func _on_button_pressed() -> void:
 	var clicked = ButtonClick.instantiate()
 	get_parent().add_child(clicked)
 	panel_sliding = true
+	button_1.hide()
+	button_2.hide()
 	start_timer.start()
 
 

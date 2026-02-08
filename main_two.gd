f118f2d63e72356061a376036a56c1f598e2f393
JTheoMitr <jmitropoulos@alumni.berklee.edu>
2026-02-07 18:13:25 -0600

    updated walls positioning
    and crystal spawn range for x and y

--- main_two.gd
+++ main_two.gd
@@ -257,8 +257,8 @@ func spawn_score_crystal_1() -> void:
 	get_parent().add_child(crystal1)
 	buff_ids.append(crystal1.get_instance_id())
 	var screen_size := get_viewport_rect().size
-	var rndX = randf_range(300, screen_size.x - 300)
-	var rndY = randf_range(300, screen_size.y - 300)
+	var rndX = randf_range(100, screen_size.x - 100)
+	var rndY = randf_range(100, screen_size.y - 100)
 	crystal1.global_position = Vector2(rndX, rndY)
 
 func spawn_impact_particles_multiplier1(pos: Vector2) -> void:

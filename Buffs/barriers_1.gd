b03825758a889ba3c8ae30a5ba8c322e6fdd23e7
JTheoMitr <jmitropoulos@alumni.berklee.edu>
2026-02-13 18:39:11 -0600

    barriers fade properly
    in sync with pyramid, mine light asset added

--- Buffs/barriers_1.gd
+++ Buffs/barriers_1.gd
@@ -1,11 +1,16 @@
 extends StaticBody2D
 
+@onready var anim1: AnimatedSprite2D = $AnimBarrier
+@onready var anim2: AnimatedSprite2D = $AnimBarrier2
+
 func _process(_delta: float) -> void:
 	self.rotation_degrees += 3
 	
 func start_fade_out() -> void:
 	var tween := create_tween()
-	tween.tween_property(self, "self_modulate:a", 0.0, 1.5)
+	var tween2 := create_tween()
+	tween.tween_property(anim1, "self_modulate:a", 0.0, 1.5)
+	tween2.tween_property(anim2, "self_modulate:a", 0.0, 1.5)
 	
 func _on_timer_timeout() -> void:
 	start_fade_out()

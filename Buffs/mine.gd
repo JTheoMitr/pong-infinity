fd1009bde6dcae200caaf3091887bccc4ce5b88f
JTheoMitr <jmitropoulos@alumni.berklee.edu>
2026-02-15 13:50:43 -0600

    mine anim flip
    minor tweaks

--- Buffs/mine.gd
+++ Buffs/mine.gd
@@ -26,7 +26,7 @@ func _ready() -> void:
 	
 func _process(_delta: float) -> void:
 	if dropping:
-		if drop_anim.position.y < 0:
+		if drop_anim.position.y < -8:
 			drop_anim.position.y += 3
 		else:
 			drop_anim.queue_free()
